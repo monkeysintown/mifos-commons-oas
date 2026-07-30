e71b08efb949f4647e811f40de7d0f43d058860e
Aleksandar Vidakovic <aleks@mifos.org>
2026-07-30 11:09:25 +0200

    chore: init

---
+++ modules/rest/sdk/pagerduty/starter/src/main/java/org/mifos/commons/oas/imperative/rest/sdk/pagerduty/starter/PagerdutyRestSdkAutoConfiguration.java
@@ -0,0 +1,21 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.mifos.commons.oas.imperative.rest.sdk.pagerduty.starter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mifos.commons.oas.imperative.rest.sdk.pagerduty.core.PagerdutyRestSdkProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+import static org.mifos.commons.oas.imperative.rest.sdk.pagerduty.core.PagerdutyRestSdkConstants.MIFOS_COMMONS_OAS_IMPERATIVE_SDK_PAGERDUTY_CORE_PACKAGE;
+import static org.mifos.commons.oas.imperative.rest.sdk.pagerduty.core.PagerdutyRestSdkConstants.MIFOS_COMMONS_OAS_IMPERATIVE_SDK_PAGERDUTY_IMPLEMENTATION_PACKAGE;
+
+@Slf4j
+@EnableConfigurationProperties({PagerdutyRestSdkProperties.class})
+@ComponentScan(MIFOS_COMMONS_OAS_IMPERATIVE_SDK_PAGERDUTY_CORE_PACKAGE)
+@ComponentScan(MIFOS_COMMONS_OAS_IMPERATIVE_SDK_PAGERDUTY_IMPLEMENTATION_PACKAGE)
+class PagerdutyRestSdkAutoConfiguration {
+    // TODO: configure client beans with authentication
+}
